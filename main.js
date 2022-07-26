b44a35cc5c2a2d8c1fc442d2a363c906a231b058
Rasmus Bremholm <rasmus.brem@gmail.com>
2022-07-26 21:51:49 +0200

    Got my dodecahedron to render!

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import './style.css'
 
 import * as THREE from 'three';
+import { TorusBufferGeometry } from 'three';
 
 const farClip = 1000;
 const nearClip = 0.1;
@@ -18,3 +19,26 @@ const renderer = new THREE.WebGLRenderer({
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+
+renderer.render(scene,camera);
+
+const geometry = new THREE.DodecahedronGeometry(10,0);
+  const material = new THREE.MeshBasicMaterial({color: 0xFF6347, wireframe: true});
+  const mesh = new THREE.Mesh(geometry,material);
+
+scene.add(mesh);
+
+
+animate();
+
+
+
+// Functions // 
+function animate(){
+    requestAnimationFrame(animate);
+    mesh.rotation.x += 0.005;
+    mesh.rotation.y += 0.005;
+
+    renderer.render(scene,camera);
+}
